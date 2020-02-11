3fc0a8078a298099cfa071f277c98f81e141eddb
Tom Bredemeier <tbredemeier@barrington220.org>
2020-02-11 13:23:28 -0600

    Parse JSON data

--- Famous Quotes/CategoriesViewController.swift
+++ Famous Quotes/CategoriesViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class CategoriesViewController: UITableViewController {
 
+    var categories = [[String: String]]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.title = "Quotation Categories"
+        let query = "https://quotes.rest/qod/categories.json"
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                let success = json["success"].dictionary
+                if success!["total"]! > 0 {
+                    parse(json: json)
+                    return
+                }
+            }
+        }
+        loadError()
     }
 
+    func parse(json: JSON) {
+        let contents = json["contents"].dictionary
+        for (key, subJson) in contents!["categories"]! {
+            categories.append([key: subJson.stringValue])
+        }
+    }
 
+    func loadError() {
+        let alert = UIAlertController(title: "Loading Error", message: "There was a problem loading the quotation categories", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true)
+    }
 }
 
